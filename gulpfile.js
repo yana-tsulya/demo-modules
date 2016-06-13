c3e63058a00f5cbe359807273e8c5ff6edf4971b
oleh.kastornov <oleh.kastornov@masterofcode.com>
2016-06-13 14:42:53 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
+var commonPath = 'src/AppBundle/Resources/public/js';
+
+gulp.task('default', ['watch']);
+
+gulp.task('build-js-dev', function () {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/requirejs/require.js',
+        'js/app/**/*.js',
+        'js/app-init.js'
+
+    ])
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('build-js-prod', function () {
+    return gulp.src([
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/requirejs/require.js',
+        'js/app/**/*.js',
+        'js/app-init.js'
+
+    ])
+        .pipe(concat('build.js'))
+        .pipe(minify({
+            ext:{
+                src:'-debug.js',
+                min:'.min.js'
+            }
+        }))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('js/**/*.js', ['build-js-dev']);
+});
